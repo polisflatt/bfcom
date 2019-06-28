85919ac73ef18b12cce72ea0dd9bd270c8962f53
Polis Flatt <30891943+polisflatt@users.noreply.github.com>
2019-06-28 05:35:44 -0500

    Wrong code

--- bftypes.py
+++ bftypes.py
@@ -24,7 +24,7 @@ main_symbols = {
     "<":"i--;",
     ".":"putchar(*i);",
     ",":"*i = getchar();",
-    "[":"while (*i != '\\0') {", # What is interesting about this is the fact that it is not exactly compatible with getchar(), as getchar() returns EOF (which is -1) and not NULL. Ergo, getchar() won't ever terminate with brainf**k.
+    "[":"while (*i) {",
     "]":"}", # Cheap way to have a while field. It works because I am abiding by the brainf**k standard.
     " ":""
 } # Not even Lexical Analysis, just minor translations of code. That's why I started with BrainF**K
